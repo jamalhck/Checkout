bbe2d6949054a20978136023a5be4bfe04e7f290
Jamal Akram <jamal.hacker16@gmail.com>
2024-11-22 03:12:56 +0000

    Created Basket.test.js to start writing different tests, Installed dependencies to start Unit testing. This will be completed in future iterations. Also deleted unnecessary files.

--- src/Backend/Calculator.js
+++ src/Backend/Calculator.js
@@ -1,16 +1,4 @@
 import products from '../Frontend/Items';
-/*
-Pseudo code from which the below function was written:
-
-click on button B, the count is added to an array
-    a loop a running in the array, when it detects more than 2 items, it does a division to 2, and saves the result (special price count = 2)
-    The count is then multiplied by 45 -> 2 * 45 = 90 (specialPrice)
-    then it checks for the remainder of that division, if it's bigger than 0, multiplied by 30 (UnitPrice)
-    else if only 1 item is scanned
-        add item price (SingleUnit)
-    SpecialPrice + UnitPrice + SingleUnit = Total
-
-*/
 
 function SpecialPriceB(itemCount) {
 
---
+++ src/Tests/Basket.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Front from '../Frontend/Front';
+
+test('Clicking Product A button updates the total price', () => {
+    //Starting test here.
+});
